3fe7f8b37049f1b5833cd7cdeec6b20322892c54
Alexis Cesaro <alexis.cesaro.ac@gmail.com>
2020-12-22 01:59:24 +0100

    Edit config files

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -5,12 +5,12 @@
  */
 
 define('INTELLI_CONNECT', getenv('CLOUD_SQL_CONNECTION_NAME'));
-define('INTELLI_DBHOST', null );
+define('INTELLI_DBHOST', getenv('CLOUD_SQL_DATABASE_HOST') );
 define('INTELLI_DBUSER', getenv('CLOUD_SQL_USERNAME'));
 define('INTELLI_DBPASS',  getenv('CLOUD_SQL_PASSWORD'));
 define('INTELLI_DBNAME',  getenv('CLOUD_SQL_DATABASE_NAME'));
 define('INTELLI_DBNAME_COMMUNITY', 'subrion');
-define('INTELLI_DBPORT', '3308');
+define('INTELLI_DBPORT', '3306');
 define('INTELLI_DBPREFIX', 'sbr421_');
 define('THELUCKYDEV_COMMUNITY_URL', 'community.theluckydev');
 
